54b342c613d6ac3c8f651b1f8a0760a3094a61c4
Angelo <delrosarioangelo18@gmail.com>
2024-05-17 11:52:30 +0800

    Add project files.

---
+++ MargotCodeSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MargotCodeSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
